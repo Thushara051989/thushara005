0b35c0f80d22f0b566f33b11dc6d09af179c57e8
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-07-09 23:44:03 +0530

    ch

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,34 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-contact',
+//   templateUrl: './contact.component.html',
+//   styleUrls: ['./contact.component.css']
+// })
+// export class ContactComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() {
+   
+  
+  }
+
+  ngOnInit(): void {
+   
+  }
+}
